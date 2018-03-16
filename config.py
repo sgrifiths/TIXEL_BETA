435944720711b1a677bc08bc717be25c929bae9e
sgrifiths <36531972+sgrifiths@users.noreply.github.com>
2018-03-16 12:07:53 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+
+""" 
+This file contains all of the configuration values for the application. 
+Update this file with the values for your specific Google Cloud project. 
+You can create and manage projects at https://console.developers.google.com 
+""" 
+ 
+import os 
+ 
+# The secret key is used by Flask to encrypt session cookies. 
+SECRET_KEY = 'secret' 
+ 
---
+++ main.py
@@ -0,0 +1,12 @@
+# copyright enhancedEPM
+
+import bp
+import config
+
+app = bp.create_app(config)
+
+# This is only used when running locally. When running live, gunicorn runs 
+# the application. 
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=8080, debug=True)
+
